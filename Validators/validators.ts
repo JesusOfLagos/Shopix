4b2fe512521f2e7b9d6d7a170af0854b96ad68d7
Dot Dev <thejesusoflagos@gmail.com>
2023-07-28 14:01:13 +0000

    I instantiated Validator

---
+++ Validators/validators.ts
@@ -0,0 +1,50 @@
+
+
+import isEmpty from 'is-empty'
+import validator from 'validator'
+
+
+module.exports.LoginValidator = (data) => {
+    const errors = {};
+
+    data.email = !(isEmpty(data.email)) ? data.email : "";
+    data.password = !(isEmpty(data.password)) ? data.password : "";
+
+    let emailError = validator.isEmpty(data.email) ? "Email is required!" : (!validator.isEmail(data.email) ? "Please provide a valid email!": "");
+    let passwordError = validator.isEmpty(data.password) ? "Passord is required": "";
+
+
+    if (emailError) errors.email = emailError;
+    if (passwordError) errors.password = passwordError;
+
+    return{
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+
+module.exports.RegisterValidator = (data) => {
+    const errors = {}
+
+    data.email = !(isEmpty(data.email)) ? data.email : "";
+    data.password = !(isEmpty(data.password)) ? data.password : "";
+    data.firstName = !(isEmpty(data.firstName)) ? data.firstName : "";
+    data.lastName = !(isEmpty(data.lastName)) ? data.lastName : "";
+
+    let emailError = validator.isEmpty(data.email) ? "Email is required!" : (!validator.isEmail(data.email) ? "Please provide a valid email!": "");
+    let passwordError = validator.isEmpty(data.password) ? "Passord is required": "";
+    let firstNameError = validator.isEmpty(data.firstName) ? "First Name is required": "";
+    let lastNameError = validator.isEmpty(data.lastName) ? "Last Name is required": "";
+
+    if (emailError) errors.email = emailError;
+    if (passwordError) errors.password = passwordError;
+    if (firstNameError || lastNameError) errors.firstName = "Full name is required";
+
+    return{
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+
